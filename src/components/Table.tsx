c4f661f8aa705b4e093eccf02af16a1de1fa42e0
Alois Carrera <aloiscarrera@gmail.com>
2021-06-19 15:31:57 -0500

    refactor: :sparkles: Add middle square method refactor

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,36 +1,40 @@
 import React from 'react';
+import type { TableData } from '../pages/MiddleSquare';
 
 type Props = {
-  seed: number;
-  iterations: number;
+  data: TableData[];
 };
 
-const Table = ({ iterations, seed }: Props) => {
-  console.log(iterations, seed);
-
-  return (
-    <table className="table-fixed mt-10 rounded-lg bg-white w-1/2">
-      <caption className="text-lg text-white mb-3">Results</caption>
-      <thead>
-        <tr>
-          <th className="py-2">n</th>
-          <th className="py-2">R(n)</th>
-          <th className="py-2">
-            R(n)<sup>2</sup>
-          </th>
-          <th className="py-2">
-            M.R(n)<sup>2</sup>
-          </th>
-          <th className="py-2">Value</th>
+const Table = ({ data: tableData }: Props) => (
+  <table className="table-fixed mt-10 rounded-lg bg-white w-1/2">
+    <caption className="text-lg text-white mb-3">Results</caption>
+    <thead>
+      <tr>
+        <th className="py-4">n</th>
+        <th className="py-4">R(n)</th>
+        <th className="py-4">
+          R(n)<sup>2</sup>
+        </th>
+        <th className="py-4">
+          M.R(n)<sup>2</sup>
+        </th>
+        <th className="py-4">Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      {tableData.map((row, idx) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <tr key={`tr${idx}`}>
+          {row.map((data, trIdx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <td className="text-center p-4" key={`td${trIdx}`}>
+              {data}
+            </td>
+          ))}
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Hola</td>
-        </tr>
-      </tbody>
-    </table>
-  );
-};
+      ))}
+    </tbody>
+  </table>
+);
 
 export default Table;
--- src/pages/MiddleSquare.tsx
+++ src/pages/MiddleSquare.tsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PageBase from '../components/PageBase';
 import MethodTitle from '../components/MethodTitle';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import Table from '../components/Table';
 
+export type TableData = [number, number, number, string, string];
+
 const MiddleSquare = (): JSX.Element => {
   const [seedAndIterations, setSeedAndIterations] = useState<[number, number]>([
     0, 0,
   ]);
+
+  const [tableData, setTableData] = useState<TableData[]>([]);
 
   const handleFormSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -20,6 +24,66 @@ const MiddleSquare = (): JSX.Element => {
 
     setSeedAndIterations([target.seed.value, target.iterations.value]);
   };
+
+  const middleSquare = (
+    generatedSeed: string,
+    squaredSeed: string,
+    middle: number,
+  ) =>
+    generatedSeed.length % 2 !== 0 && squaredSeed.length % 2 === 0
+      ? squaredSeed.substr(middle - 1, generatedSeed.length)
+      : squaredSeed.substr(middle, generatedSeed.length);
+
+  const wordMiddleIndex = (longerWord: string, shorterWord: string) => {
+    const result = longerWord.length - shorterWord.length;
+
+    return result / 2;
+  };
+
+  const getMethodResult = () => {
+    const dataResult: TableData[] = [];
+
+    let seed = seedAndIterations[0];
+
+    for (let index = 0; index < seedAndIterations[1]; index += 1) {
+      const seedSquared = seed ** 2;
+
+      const middleIndex = wordMiddleIndex(
+        seedSquared.toString(),
+        seed.toString(),
+      );
+
+      const result = middleSquare(
+        seed.toString(),
+        seedSquared.toString(),
+        middleIndex,
+      );
+
+      const data: [number, number, number, string, string] = [
+        index,
+        seed,
+        seedSquared,
+        result,
+        result,
+      ];
+
+      const newSeed = middleSquare(
+        seed.toString(),
+        seedSquared.toString(),
+        middleIndex,
+      );
+
+      seed = parseInt(newSeed, 10);
+
+      dataResult.push(data);
+    }
+
+    setTableData(dataResult);
+  };
+
+  useEffect(() => {
+    getMethodResult();
+  }, [seedAndIterations]);
 
   return (
     <PageBase bgColor="bg-green-600">
@@ -34,7 +98,7 @@ const MiddleSquare = (): JSX.Element => {
           <Button text="Clear" />
         </div>
       </form>
-      <Table iterations={seedAndIterations[0]} seed={seedAndIterations[1]} />
+      <Table data={tableData} />
     </PageBase>
   );
 };
