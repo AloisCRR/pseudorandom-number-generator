8cf6186358c250960db6bb56c4d99173cc808e34
Alois Carrera <aloiscarrera@gmail.com>
2021-06-19 21:18:37 -0500

    refactor: :art: Refactor middle square page to add custom hook

--- src/pages/MiddleSquare.tsx
+++ src/pages/MiddleSquare.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import PageBase from '../components/PageBase';
-import MethodTitle from '../components/MethodTitle';
-import Input from '../components/Input';
+import React from 'react';
 import Button from '../components/Button';
+import Input from '../components/Input';
+import MethodTitle from '../components/MethodTitle';
+import PageBase from '../components/PageBase';
 import Table from '../components/Table';
-
-export type TableData = [number, number, number, string, string];
+import usePseudorandomNumberGenerator from '../hooks/usePseudorandomNumberGenerator';
 
 const MiddleSquare = (): JSX.Element => {
-  const [seedAndIterations, setSeedAndIterations] = useState<[number, number]>([
-    0, 0,
-  ]);
-
-  const [tableData, setTableData] = useState<TableData[]>([]);
+  const { tableData, setSeedAndIterations } = usePseudorandomNumberGenerator({
+    iterations: 0,
+    method: 'middle-square',
+    seed: { seedOne: 0 },
+  });
 
   const handleFormSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -22,68 +21,12 @@ const MiddleSquare = (): JSX.Element => {
       iterations: { value: number };
     };
 
-    setSeedAndIterations([target.seed.value, target.iterations.value]);
+    setSeedAndIterations({
+      iterations: 0,
+      method: 'middle-square',
+      seed: { seedOne: target.seed.value },
+    });
   };
-
-  const middleSquare = (
-    generatedSeed: string,
-    squaredSeed: string,
-    middle: number,
-  ) =>
-    generatedSeed.length % 2 !== 0 && squaredSeed.length % 2 === 0
-      ? squaredSeed.substr(middle - 1, generatedSeed.length)
-      : squaredSeed.substr(middle, generatedSeed.length);
-
-  const wordMiddleIndex = (longerWord: string, shorterWord: string) => {
-    const result = longerWord.length - shorterWord.length;
-
-    return result / 2;
-  };
-
-  const getMethodResult = () => {
-    const dataResult: TableData[] = [];
-
-    let seed = seedAndIterations[0];
-
-    for (let index = 0; index < seedAndIterations[1]; index += 1) {
-      const seedSquared = seed ** 2;
-
-      const middleIndex = wordMiddleIndex(
-        seedSquared.toString(),
-        seed.toString(),
-      );
-
-      const result = middleSquare(
-        seed.toString(),
-        seedSquared.toString(),
-        middleIndex,
-      );
-
-      const data: [number, number, number, string, string] = [
-        index,
-        seed,
-        seedSquared,
-        result,
-        result,
-      ];
-
-      const newSeed = middleSquare(
-        seed.toString(),
-        seedSquared.toString(),
-        middleIndex,
-      );
-
-      seed = parseInt(newSeed, 10);
-
-      dataResult.push(data);
-    }
-
-    setTableData(dataResult);
-  };
-
-  useEffect(() => {
-    getMethodResult();
-  }, [seedAndIterations]);
 
   return (
     <PageBase bgColor="bg-green-600">
